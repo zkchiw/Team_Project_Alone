a4bd72af32b99aef3919d561918d3311251381da
zkchiw <zkchiw@naver.com>
2022-07-11 17:24:53 +0900

    복기

---
+++ static/css/vendor/js/DateSelector.js
@@ -0,0 +1,26 @@
+  function goThere(){
+        const dater = document.querySelector('#date')
+        jk = dater.options[dater.selectedIndex]
+            if (jk.value==="july_all"){
+                location.href = "july_all"}
+            if(jk.value==="july_02"){
+                location.href = "july_02"}
+            if (jk.value==="july_03"){
+                location.href = "july_03"}
+            if (jk.value==="july_05"){
+                location.href = "july_05"}
+            if (jk.value==="july_06"){
+                location.href = "july_06"}
+            if (jk.value==="july_08"){
+                location.href = "july_08"}
+            if (jk.value==="july_09"){
+                location.href = "july_09"}
+            if (jk.value==="july_10"){
+                location.href = "july_10"}
+            if (jk.value==="july_16"){
+                location.href = "july_16"}
+            if (jk.value==="july_30"){
+                location.href = "july_30"}
+            if (jk.value==="july_31"){
+                location.href = "july_31"}}
+
